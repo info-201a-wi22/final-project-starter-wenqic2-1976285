7056f88524da2d56828cde2d73b1c0619711a0a3
Andy Cerekja <acerekja@uw.edu>
2022-03-11 19:15:14 -0800

    applies completed summary ui

--- p03/summary_app_ui.R
+++ p03/summary_app_ui.R
@@ -7,20 +7,18 @@ library(plotly)
 Summary <- tabPanel(
   h4("Summary"),
   h1("Summary"),
-  p("1) By analyzing our first set of graphs, it can be seen that since 1990, there has been a large
+  p("By analyzing our first set of graphs, it can be seen that since 1990, there has been a large
     increase in the total number of people living with HIV globally. Most of these cases seem to be
     coming from Africa, which is the only region to see a consistent sharp increase in cases along
     this time period, while the other regions have much smaller slopes and stayed more consistent.
     This increase in cases showcases several things: It shows how much better our HIV treatment has
     got in order to keep more people alive for longer periods of time, but it also shows how we have
     yet to improve, in a global standpoint, in terms of slowing the spread of it."),
-  p("2) Through analyzing our second set of graphs, we can see that as of 2005 there has been a
+  p("Through analyzing our second and third sets of graphs, we can see that as of 2005 there has been a
     continuous downwards trend in the global deaths due to HIV, despite the previous graph showing
     that we have continued to increase cases globally over the years. This is a good sign, as it shows
     the effectiveness of modern HIV treatment and the dispersion of that treatment globally to
-    limit the number of people who die of HIV every year."),
-  p("*Pending third graph information")
-  
+    limit the number of people who die of HIV every year.")
 )
 
 app_ui <- navbarPage(
